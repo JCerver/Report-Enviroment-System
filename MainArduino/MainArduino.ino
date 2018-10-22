d4bc645636c4f347131e1037c4900a1132097fa6
Miguel Ángel Ramírez <mikeangelsoldier1@gmail.com>
2018-10-21 19:20:29 -0500

    modificacionde prueba del main arduino

--- MainArduino/MainArduino.ino
+++ MainArduino/MainArduino.ino
@@ -15,16 +15,16 @@
 void setup() {
   //setupTecladoMatricial();
   //setupDisplay();
-  setupSensorLuz();
-  //setupSensorTemperatura();
+  //setupSensorLuz();
+  setupSensorTemperatura();
   //setupSensorHumedad();
 }
 
 void loop() {
   //loopTecladoMatricial();
   //loopDisplay();
-  loopSensorLuz();
-  //loopSensorTemperatura();
+  //loopSensorLuz();
+  loopSensorTemperatura();
   //loopSensorHumedad();
 
 }
